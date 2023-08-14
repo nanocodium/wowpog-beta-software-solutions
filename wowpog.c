d0a78a01f28f73d8d4a710551863d1cdd259e493
killingspree69 <dickheadjhon23@gmail.com>
2023-08-14 01:43:08 +0100

    Added wowpog.c source code

---
+++ wowpog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+	int choice;
+	system("clear");
+	printf("WoW Pog Beta 0.1\n");
+	printf("\n1. Connect to wowpog server\n");
+	printf("Type a number and hit enter: ");
+	scanf(" %d", &choice);	
+	switch (choice) {
+		case 1:
+		system("echo [!] Error wowpog servers not online .....");
+		int choice = 0;
+	}
+	return 0;	
+}
